0cb0e38f4fb30baa093224e4ea6a1297d4c0678b
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-27 16:59:39 +0100

    Ajout d'un input name

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import './style.css'
+import './style.css';
+import dotenv from 'dotenv';
 
-// API pour le mail
-{
-    "email": "julesmukadi.dev@gmail.com",
-        "project_id": "6a98ce26-600b-47a7-a2fb-628c8bb22a8b",
-            "private_key": "smad2502241055428a16610a10",
-                "name": "Jules MUKADI" //  Optional, only if "add user name field" is enabled when you create the project, else set null
-}
+dotenv.config(); // Charge les variables d'environnement
+
+const apiConfig = {
+    project_id: process.env.PROJECT_ID,
+    private_key: process.env.PRIVATE_KEY,
+};
 
 async function saveEmailToMailingList(email, name) {
     const response = await fetch('https://api.smadmail.com/api/v1/email/save', {
@@ -16,10 +16,10 @@ async function saveEmailToMailingList(email, name) {
         },
         body: JSON.stringify({
             email: email,
-            project_id: process.env.PROJECT_ID,
-            private_key: process.env.PRIVATE_KEY,
-            name: name //  Optional, only if "add user name field" is enabled when you create the project, else set null
-        })
+            project_id: apiConfig.project_id,
+            private_key: apiConfig.private_key,
+            name: name || null,
+        }),
     });
 
     if (!response.ok) {
