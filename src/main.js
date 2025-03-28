d7686e68ef66df9e62d545ba7c81b61760295cfb
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-28 12:56:23 +0100

    Ajout de l'API smad

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import './style.css';
-import dotenv from 'dotenv';
 
-dotenv.config(); // Charge les variables d'environnement
-
-const apiConfig = {
-    project_id: process.env.PROJECT_ID,
-    private_key: process.env.PRIVATE_KEY,
-};
+{
+    email: "julesmukadi.dev@gmail.com";
+    project_id: "1b95525d-303f-422a-890e-fbb5267554ce";
+    private_key: "smad2502241055428a16610a10";
+    name: "Jules MUKADI"; // Optional, only if "add user name field" is enabled when you create the project
+}
 
 async function saveEmailToMailingList(email, name) {
     const response = await fetch('https://api.smadmail.com/api/v1/email/save', {
@@ -16,10 +15,10 @@ async function saveEmailToMailingList(email, name) {
         },
         body: JSON.stringify({
             email: email,
-            project_id: apiConfig.project_id,
-            private_key: apiConfig.private_key,
-            name: name || null,
-        }),
+            project_id: process.env.PROJECT_ID,
+            private_key: process.env.PRIVATE_KEY,
+            name: name //  Optional, only if "add user name field" is enabled when you create the project, else set null
+        })
     });
 
     if (!response.ok) {
